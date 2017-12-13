2184c42509dcd0f964a99d643d5b32f99182b350
Tobias Jansäter <tobias.jansater@rapidsolutions.se>
2017-12-13 11:10:52 +0100

    Added file reference id to attachments

--- TypeLess.Mail/Attachment.cs
+++ TypeLess.Mail/Attachment.cs
@@ -12,7 +12,8 @@ namespace TypeLess.Mail
         public byte[] Content { get; set; }
         public string Name { get; set; }
         public string MediaType { get; set; }
-        
+        public int FileReferenceId { get; set; }
+
         public Attachment(byte[] content, string name, string mediaType)
         {
             this.Content = content;
@@ -20,7 +21,17 @@ namespace TypeLess.Mail
             this.MediaType = mediaType;
         }
 
+        public Attachment(int fileReferenceId)
+        {
+            this.FileReferenceId = fileReferenceId;
+        }
+
         internal Stream OpenContentStream() {
+
+            if (Content == null) {
+                throw new ApplicationException("Content has not been set");
+            }
+
             return new MemoryStream(Content);
         }
         
